16656efefc9aa2633986223cee55213dc3ceb64f
Anton K <ant.korotun@gmail.com>
2025-08-15 19:50:13 +0300

    added routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
+import { inject } from '@angular/core';
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { UserService } from './core/services/user.service';
+import { map } from 'rxjs';
+import { authGuard } from './core/auth/auth.guard';
+export const routes: Routes = [
+    
+    {
+      path: '',
+      loadComponent:() => import("./features/calendar/calendar").then(m => m.Calendar),
+      canActivate: [authGuard],
+    },
+    {
+      path: "auth",
+      loadComponent:() => import("./core/auth/auth").then(m => m.Auth),
+      canActivate: [
+      () => inject(UserService).isAuthenticated.pipe(map((isAuth) => !isAuth)),
+      ],
+    }
+];
