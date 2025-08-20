2ac5b49ca6939a1a398018ab2f5f02221c1835fb
Anton K <ant.korotun@gmail.com>
2025-08-20 18:58:48 +0300

    added navbar UI

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./shared/components/navbar/navbar";
 import { UserService } from './core/services/user.service';
 import { AsyncPipe } from '@angular/common';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, AsyncPipe],
+  imports: [
+    RouterOutlet, 
+    Navbar, 
+    AsyncPipe, 
+    NzMenuModule,
+    NzIconModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
