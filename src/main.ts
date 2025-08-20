3281929ead03c3bb2857ba319b3d42c70901de57
Anton K <ant.korotun@gmail.com>
2025-08-20 18:58:12 +0300

    also antd

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { jwtInterceptor } from './app/core/interceptors/jwt.interceptor';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { importProvidersFrom } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+registerLocaleData(en);
 
 bootstrapApplication(App, {
  providers:[
@@ -13,7 +21,7 @@ bootstrapApplication(App, {
     withInterceptors([jwtInterceptor])
   ),
   provideRouter(routes),
-  provideAnimations()
+  provideAnimations(), provideNzI18n(en_US), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient()
  ] 
 })
   .catch((err) => console.error(err));
