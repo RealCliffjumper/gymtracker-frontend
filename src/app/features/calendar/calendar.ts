fa0c92f1d3c809604d1d9d9b35b4ce9df411c376
Anton K <ant.korotun@gmail.com>
2025-08-22 20:27:18 +0300

    added calendar UI template

--- src/app/features/calendar/calendar.ts
+++ src/app/features/calendar/calendar.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NzCalendarModule, NzCalendarMode } from 'ng-zorro-antd/calendar';
 
 @Component({
   selector: 'app-calendar',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    NzCalendarModule
+  ],
   templateUrl: './calendar.html',
   styleUrl: './calendar.css'
 })
 export class Calendar {
 
+  date = new Date(2025, 7, 1);
+  mode: NzCalendarMode = 'month';
+
+  panelChange(change: { date: Date; mode: string }): void {
+    console.log(change.date, change.mode);
+  }
 }
