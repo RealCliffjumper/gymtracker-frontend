939e65d33f96cb30054b49764c7f13842a817ec4
Anton K <ant.korotun@gmail.com>
2025-09-04 19:40:09 +0300

    added custom date pipe

---
+++ src/app/shared/pipes/parse-dates-pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ParseDatesPipe } from './parse-dates-pipe';
+
+describe('ParseDatesPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ParseDatesPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
