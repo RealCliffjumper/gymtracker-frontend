6373feaeec6aa7838ab841efa4d6aa76cef2caf8
Anton K <ant.korotun@gmail.com>
2025-12-18 14:14:51 +0200

    scheduled workout batch edit support

--- src/app/shared/models/scheduledupdate.dto.ts
+++ src/app/shared/models/scheduledupdate.dto.ts
@@ -1,10 +1,10 @@
-import { NonNullableFormBuilder } from "@angular/forms";
-import { ScheduledExercise } from "./scheduledexercise.dto";
+import { ScheduledExerciseUpdate } from "./scheduledexerciseupdate.dto";
 
 export interface ScheduledUpdate{
     workoutNotes: string,
     status: string,
     startedAt: Date | null,
     completedAt: Date | null,
-    exercises: ScheduledExercise[]
+    exercises: ScheduledExerciseUpdate[]
+    workoutPoints?: number
 }
