55f9a1f9528db74bc10014ab4374d7ddf43d1f68
Anton K <ant.korotun@gmail.com>
2026-02-03 17:57:36 +0200

    minor change

--- src/app/shared/models/equipment-dictionary.ts
+++ src/app/shared/models/equipment-dictionary.ts
@@ -1,7 +1,7 @@
 export const EQUIPMENT_TYPE_LABELS: Record<string, string> = {
   BARBELL: 'Barbell',
   KETTLEBELL: 'Kettlebell',
-  BODYWEIGHT: 'Bodyweight',
+  BODYWEIGHT: 'None (Bodyweight)',
   DUMBBELL: 'Dumbbell',
   MACHINE: 'Machine',
 };
