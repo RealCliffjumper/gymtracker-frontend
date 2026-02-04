1d5d9e419b1324cdadc58042a51c1391a537bd91
Anton K <ant.korotun@gmail.com>
2026-02-04 18:06:09 +0200

    installed canvas-confetti

---
+++ src/app/shared/utils/confetti.util.ts
@@ -0,0 +1,28 @@
+import confetti from 'canvas-confetti';
+
+export function fireConfetti() {
+  const duration = 600;
+  const end = Date.now() + duration;
+
+  const frame = () => {
+    confetti({
+      particleCount: 4,
+      angle: 60,
+      spread: 55,
+      origin: { x: 0, y: 0.6 }
+    });
+
+    confetti({
+      particleCount: 4,
+      angle: 120,
+      spread: 55,
+      origin: { x: 1, y: 0.6 }
+    });
+
+    if (Date.now() < end) {
+      requestAnimationFrame(frame);
+    }
+  };
+
+  frame();
+}
